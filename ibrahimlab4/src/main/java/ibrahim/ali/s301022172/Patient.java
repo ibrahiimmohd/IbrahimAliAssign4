1d8c66dd76d6c9b9834e34e0501d181ae98de4ee
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2020-11-11 20:17:20 -0500

    Added PatientDb

---
+++ ibrahimlab4/src/main/java/ibrahim/ali/s301022172/Patient.java
@@ -0,0 +1,53 @@
+package ibrahim.ali.s301022172;
+
+public class Patient {
+    //private fields
+    private int _patientId;
+    private String _patientName;
+    private int _patientGender;
+    private String _patientDepartment;
+
+    //no argument constructor
+    public Patient() {}
+
+    //three argument constructor
+    public Patient(int patientId, String patientName, int patientGender, String patientDepartment) {
+        this._patientId = patientId;
+        this._patientName = patientName;
+        this._patientGender = patientGender;
+        this._patientDepartment = patientDepartment;
+    }
+
+    //getter and setter methods
+    public int getPatientId() {
+        return _patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this._patientId = patientId;
+    }
+
+    public String getPatientName() {
+        return _patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this._patientName = patientName;
+    }
+
+    public int getPatientGender() {
+        return _patientGender;
+    }
+
+    public void setPatientGender(int patientGender) {
+        this._patientGender = patientGender;
+    }
+
+    public String getPatientDepartment() {
+        return _patientDepartment;
+    }
+
+    public void setPatientDepartment(String patientDepartment) {
+        this._patientDepartment = patientDepartment;
+    }
+}
